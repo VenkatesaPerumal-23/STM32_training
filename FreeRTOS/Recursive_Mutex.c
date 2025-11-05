4f3bfe40374146581071ecbc980064ace455b76c
VenkatesaPerumal <122686745+VenkatesaPerumal-23@users.noreply.github.com>
2025-11-05 16:17:03 +0530

    Add FreeRTOS recursive mutex implementation
    This file implements a FreeRTOS application that utilizes a recursive mutex for safe printing from multiple tasks. It includes task creation and a main function to initialize the system and start the scheduler.

---
+++ FreeRTOS/Recursive_Mutex.c
@@ -0,0 +1,190 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "Semphr.h"
+#include <stdio.h>
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+extern void initialise_monitor_handles(void);
+
+SemaphoreHandle_t xRecursiveMutex;
+
+void safe_printf(char* string){
+
+	xSemaphoreTakeRecursive(xRecursiveMutex,portMAX_DELAY);
+
+	printf("%s\n",string);
+
+	xSemaphoreGiveRecursive(xRecursiveMutex);
+}
+
+void Task1(void *argument)
+{
+    for (;;)
+    {
+        safe_printf("Task1: starting operation...\n");
+
+        vTaskDelay(pdMS_TO_TICKS(500));
+
+        xSemaphoreTakeRecursive(xRecursiveMutex, portMAX_DELAY);
+
+        safe_printf("Task1: nested safe print using recursive mutex\n");
+
+        xSemaphoreGiveRecursive(xRecursiveMutex);
+
+    }
+}
+
+void Task2(void *argument)
+{
+    for (;;)
+    {
+        safe_printf("Task2: running periodic operation...\n");
+
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+int main(void)
+{
+
+  initialise_monitor_handles();
+
+  HAL_Init();
+
+  SystemClock_Config();
+
+  MX_GPIO_Init();
+
+  printf("FreeRTOS Recursive Mutex\n");
+
+  xRecursiveMutex=xSemaphoreCreateRecursiveMutex();
+
+  if(xRecursiveMutex==NULL){
+
+	  printf("Failed to create it\n");
+	  while(1);
+  }
+
+
+  /* Create two tasks */
+  xTaskCreate(Task1, "Task1", 128, NULL, 2, NULL);
+  xTaskCreate(Task2, "Task2", 128, NULL, 2, NULL);
+
+  /* Start scheduler */
+  vTaskStartScheduler();
+
+  for(;;);
+}
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_GPIO_Init(void)
+{
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+}
+
+
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM5)
+  {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
